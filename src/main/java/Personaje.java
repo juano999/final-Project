7a46ed636bb840d93ef86eed677d74d782558e54
Karen181505 <66264779+Karen181505@users.noreply.github.com>
2020-09-09 12:10:30 -0500

    Add files via upload

---
+++ src/main/java/Personaje.java
@@ -0,0 +1,32 @@
+
+import java.util.ArrayList;
+import javafx.scene.image.Image;
+
+public class Personaje {
+
+    private String name;
+    private int vida;
+    private int estamina;
+    private int ataque;
+    private int ataqueFinal;
+    private Image image;
+    
+    
+    public Personaje() {
+
+    }
+
+    public void newCharacter(ArrayList<String> characters) {
+        this.name = characters.get(0);
+        this.vida = Integer.valueOf(characters.get(1));
+        this.estamina =Integer.valueOf(characters.get(2));
+        this.ataque =Integer.valueOf(characters.get(3));
+        this.ataqueFinal = Integer.valueOf(characters.get(4));
+        this.image = new Image(characters.get(5));
+    }
+    
+    
+    
+    
+
+}
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,15 +1,70 @@
 
+import java.util.ArrayList;
+
+
 public class Player {
     private String name;
     private String lastName;
-    private int id;
-    private int ced;
-
-    public Player(String name, String lastName, int id, int ced) {
+    private String cedula;
+    private String id;
+    private String victories;
+    private Personaje personaje;
+    private final ArrayList<String> personaje1 = new ArrayList<>();
+    private final ArrayList<String> personaje2 = new ArrayList<>();
+    private final ArrayList<String> personaje3 = new ArrayList<>();
+    private final ArrayList<String> personaje4 = new ArrayList<>();
+    
+    public Player(String name, String lastName, String cedula, String id, String victorias){
         this.name = name;
         this.lastName = lastName;
+        this.cedula = cedula;
         this.id = id;
-        this.ced = ced;
+        personaje1.add("");
+        personaje1.add("");
+        personaje1.add("");
+        personaje1.add("");
+        personaje1.add("");
+        personaje2.add("");
+        personaje2.add("");
+        personaje2.add("");
+        personaje2.add("");
+        personaje3.add("");
+        personaje3.add("");
+        personaje3.add("");
+        personaje3.add("");
+        personaje4.add("");
+        personaje4.add("");
+        personaje4.add("");
+        personaje4.add("");
+        personaje4.add("");
     }
-  
+    
+    public ArrayList<String> character(String name){
+        if(name.equals(this.personaje1.get(0))){
+            return this.personaje1;
+        }
+        if(name.equals(this.personaje2.get(0))){
+            return this.personaje1;
+        }
+        if(name.equals(this.personaje3.get(0))){
+            return this.personaje1;
+        }
+        if(name.equals(this.personaje4.get(0))){
+            return this.personaje1;
+        }
+        return null;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public  String  getCedula(){
+        return this.cedula;
+    }
+    public  String  getId(){
+        return this.id;
+    }
 }
